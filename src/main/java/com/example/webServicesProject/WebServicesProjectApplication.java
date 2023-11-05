04c73eff83f50ddd1757bb95d92c6a9109a284e1
Vinicius Gabriel <vinicius.gabriel83@yahoo.com>
2023-11-05 14:27:55 -0300

    Project created

---
+++ src/main/java/com/example/webServicesProject/WebServicesProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.webServicesProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webServicesProject/WebServicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webServicesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
